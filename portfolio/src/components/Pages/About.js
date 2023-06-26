55f07d37d2dfa0275d5574195f619d3fd2c493a0
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2023-06-25 20:39:42 -0400

    Adding pics to the website

--- portfolio/src/components/Pages/About.js
+++ portfolio/src/components/Pages/About.js
@@ -1,10 +1,17 @@
-import React, { useRef } from "react";
+import React, { useRef, useMemo } from "react";
+import {useState} from 'react';
 import { Link } from "react-scroll";
-import ScrollToTop from "./components/ScrollToTop";
 import "./Styles/About.css";
 import { BsFillBookFill } from "react-icons/bs";
 import { TfiHummer } from "react-icons/tfi";
 import { FaRegGrinAlt } from "react-icons/fa";
+import useMeasure from 'react-use-measure';
+import { useTransition, a } from '@react-spring/web';
+import shuffle from 'lodash.shuffle';
+import Masonry from './components/Introduction.tsx';
+
+
+
 
 export const About = () => {
   const Introduction = useRef(null);
@@ -19,32 +26,82 @@ export const About = () => {
     }
   };
 
+  const [isHoveringIntro, setIsHoveringIntro] = useState(false);
+  const [isHoveringEdu, setIsHoveringEdu] = useState(false);
+  const [isHoveringExp, setIsHoveringExp] = useState(false);
+
+  const handleIntroMouseOver = () => {
+    setIsHoveringIntro(true);
+  };
+
+  const handleIntroMouseOut = () => {
+    setIsHoveringIntro(false);
+  };
+
+  const handleEduMouseOver = () => {
+    setIsHoveringEdu(true);
+  };
+
+  const handleEduMouseOut = () => {
+    setIsHoveringEdu(false);
+  };
+
+  const handleExpMouseOver = () => {
+    setIsHoveringExp(true);
+  };
+
+  const handleExpMouseOut = () => {
+    setIsHoveringExp(false);
+  };
+
   return (
     <div className="About">
-      <ScrollToTop />
       <div className="Container">
         <ul>
-          <li onClick={() => scrollToSection(Introduction)} className="link nav-item">
-            <FaRegGrinAlt />
+          <li
+            onClick={() => scrollToSection(Introduction)}
+            className="link nav-item icon"
+            onMouseOver={handleIntroMouseOver}
+            onMouseOut={handleIntroMouseOut}
+          >
+            <div className="icon-container">
+              <FaRegGrinAlt />
+              {isHoveringIntro && <span className="tooltip">{"                               "}Introduction</span>}
+            </div>
           </li>
           <li>.</li>
           <li>.</li>
 
-          <li onClick={() => scrollToSection(Education)} className="link nav-item">
-          <BsFillBookFill />
+          <li
+            onClick={() => scrollToSection(Education)}
+            className="link nav-item icon"
+            onMouseOver={handleEduMouseOver}
+            onMouseOut={handleEduMouseOut}
+          >
+            <div className="icon-container">
+              <BsFillBookFill />
+              {isHoveringEdu && <span className="tooltip">{"                               "}Education</span>}
+            </div>
           </li>
 
           <li>.</li>
           <li>.</li>
 
-          <li onClick={() => scrollToSection(Experiance)} className="link nav-item">
-            <TfiHummer />
+          <li
+            onClick={() => scrollToSection(Experiance)}
+            className="link nav-item icon"
+            onMouseOver={handleExpMouseOver}
+            onMouseOut={handleExpMouseOut}
+          >
+            <div className="icon-container">
+              <TfiHummer />
+              {isHoveringExp && <span className="tooltip">{"                               "}Experiance</span>}
+            </div>
           </li>
-          
         </ul>
       </div>
       <div ref={Introduction} className="Introduction">
-        <h3>Introduction</h3>
+        <h3><Masonry /></h3>
       </div>
 
       <div ref={Education} className="Education">
@@ -57,4 +114,9 @@ export const About = () => {
     </div>
   );
 };
+
+
+
+/* Random Pics */
+
 
