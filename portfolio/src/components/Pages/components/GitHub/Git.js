149b8a95b154a9c3127e63a26d8368be4e51651d
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2023-06-27 22:41:14 -0400

    fa

--- portfolio/src/components/Pages/components/GitHub/Git.js
+++ portfolio/src/components/Pages/components/GitHub/Git.js
@@ -9,8 +9,8 @@ function Git() {
   return (
     <div className="card5">
         <div class="card-inner">
-        <div class="card-front5">
-        <FaGithub  className="card-icon5"/>
+        <div class="card-front5 ">
+        <FaGithub  className="card-icon5 "/>
          </div>
      <div class="card-back5">
             <div className="card-border-top5"></div>
