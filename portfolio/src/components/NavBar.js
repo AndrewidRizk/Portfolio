bbc8e565c9d817ad94121c308dff5c4b882e4004
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2023-11-01 19:55:05 -0400

    fixing home page on phone

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,36 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../Styles/NavBar.css";
-import {Icon} from 'react-icons-kit'
-import {menu} from 'react-icons-kit/feather/menu'
-import {x} from 'react-icons-kit/feather/x'
+import { Icon } from 'react-icons-kit';
+import { menu } from 'react-icons-kit/feather/menu';
+import { x } from 'react-icons-kit/feather/x';
 import { HiOutlineHome } from "react-icons/hi";
 
-
 function NavBar() {
-
-  const [toggle, setToggle]=useState(false);
-
-  const handleToggle=()=>{
-    setToggle(!toggle);
-  }
-
-
-
+  const [toggle, setToggle] = useState(false);
   const [click, setClick] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
   const handleClick = () => setClick(!click);
 
+  // Update the windowWidth when the window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
 
-  
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-
-    <nav className={toggle?'  expanded ':'nav-menu'}>
-
-        <div className="nav-container wrapper header">
+    <nav className={toggle ? 'expanded ' : 'nav-menu'}>
+      <div className="nav-container wrapper header">
+        {windowWidth <= 768 && ( // Only show toggle icon on phones
+          <nav className={toggle ? 'expanded ' : 'nav-menu'}>
           
-        <ul className='links dropdown'>
-
+            <div className='toggle-icon' onClick={handleToggle}>
+              {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={30} />}
+            </div>
+            <ul className='links dropdown'>
+              {toggle && ( // Only render the links when toggle is true
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      exact
+                      to="/"
+                      activeClassName="active"
+                      className="nav-links"
+                      onClick={handleClick}
+                    >
+                      <HiOutlineHome /> Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item dropbtn">
+                    <NavLink
+                      exact
+                      to="/about"
+                      activeClassName="active"
+                      className="nav-links"
+                      onClick={handleClick}
+                    >
+                      About Me
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      exact
+                      to="/skills"
+                      activeClassName="active"
+                      className="nav-links"
+                      onClick={handleClick}
+                    >
+                      Qualification
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      exact
+                      to="/projects"
+                      activeClassName="active"
+                      className="nav-links"
+                      onClick={handleClick}
+                    >
+                      Projects
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      exact
+                      to="/contact"
+                      activeClassName="active"
+                      className="nav-links"
+                      onClick={handleClick}
+                    >
+                      Contact Me
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          
+        </nav>
+        )}
+        {windowWidth > 768 && ( // Render links only on larger screens
+          <ul className='links dropdown'>
             <li className="nav-item ">
               <NavLink
                 exact
@@ -39,10 +113,9 @@ function NavBar() {
                 className="nav-links"
                 onClick={handleClick}
               >
-                <HiOutlineHome/> Home
+                <HiOutlineHome /> Home
               </NavLink>
             </li>
-
             <li className="nav-item dropbtn">
               <NavLink
                 exact
@@ -53,9 +126,7 @@ function NavBar() {
               >
                 About Me
               </NavLink>
-              
             </li>
-
             <li className="nav-item">
               <NavLink
                 exact
@@ -67,7 +138,6 @@ function NavBar() {
                 Qualification
               </NavLink>
             </li>
-
             <li className="nav-item">
               <NavLink
                 exact
@@ -79,8 +149,6 @@ function NavBar() {
                 Projects
               </NavLink>
             </li>
-
-
             <li className="nav-item">
               <NavLink
                 exact
@@ -92,14 +160,9 @@ function NavBar() {
                 Contact Me
               </NavLink>
             </li>
-            
-            <div className='toggle-icon' onClick={handleToggle}>
-              {toggle?<Icon icon={x} size={28}/>:<Icon icon={menu} size={30}/>}
-            </div>
-
-            </ul>
-
-        </div>
+          </ul>
+        )}
+      </div>
     </nav>
   );
 }
