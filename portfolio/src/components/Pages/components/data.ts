2b6d68c05f8992ad99326b04d6b7e7535af3959a
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2023-11-03 11:24:04 -0400

    add some pics

--- portfolio/src/components/Pages/components/data.ts
+++ portfolio/src/components/Pages/components/data.ts
@@ -1,6 +1,6 @@
-
+import theBoys from './photos/theBoys.PNG';
 export default [
-  { css: 'https://images.pexels.com/photos/416430/pexels-photo-416430.jpeg', height: 250 },
+  { css: {theBoys}, height: 250 },
   { css: 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg', height: 400 },
   { css: 'https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg', height: 400 },
   { css: 'https://images.pexels.com/photos/358574/pexels-photo-358574.jpeg', height: 400 },
